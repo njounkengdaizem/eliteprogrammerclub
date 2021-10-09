97b863beeff327b71c45eba4b2ae9cc595df0178
njounkengdaizem <njounkengdaizem@berea.edu>
2021-10-08 23:46:30 -0400

    modified capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -3,12 +3,15 @@
 #############################################################
 result = ""     # an empty string to hold the resulting string
 sentence = input("Enter a word: ")                                  # gets inputs from the user
-sentence = sentence.capitalize()                                        # capitalizes the first letter of the string
-sentence = sentence.split('.')                                          # splits words depending on where the "." is
+sentence = sentence.capitalize()
+sentence_list = sentence.split('.')
+list = []
 
-for word in sentence:                                                  # here goes the magic
+for word in sentence_list:
+    list.append(word.strip())                               # takes out every whitespace between splited sentence
+sentence = list
+
+for word in sentence:                                    # here goes the magic
     result += word[0].upper() + word[1:] + '. '          # converts the first letter of the split string to uppercase
 print(result[:])                                   # then concatenates the remaining letters and adds a
                                                    # full stop and empty space between the words.
-
-
