de25e29c263a3473f263a75fb01a3390aaf2b273
njounkengdaizem <njounkengdaizem@berea.edu>
2021-10-08 21:30:42 -0400

    edit variable names

--- capitalize.py
+++ capitalize.py
@@ -2,12 +2,12 @@
 # returns the capitalized for of the sentence.
 #############################################################
 result = ""     # an empty string to hold the resulting string
-word = input("Enter a word: ")                                  # gets inputs from the user
-word = word.capitalize()                                        # capitalizes the first letter of the string
-word = word.split('.')                                          # splits words depending on where the "." is
+sentence = input("Enter a word: ")                                  # gets inputs from the user
+sentence = sentence.capitalize()                                        # capitalizes the first letter of the string
+sentence = sentence.split('.')                                          # splits words depending on where the "." is
 
-for i in word:                                                  # here goes the magic
-    result += i[0].upper() + i[1:] + '. '          # converts the first letter of the split string to uppercase
+for word in sentence:                                                  # here goes the magic
+    result += word[0].upper() + word[1:] + '. '          # converts the first letter of the split string to uppercase
 print(result[:])                                   # then concatenates the remaining letters and adds a
                                                    # full stop and empty space between the words.
 
